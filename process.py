381f7ddd50531c9c4d662758f517eff2a354c287
Vladislav Savionok <savionak@gmail.com>
2017-03-12 23:07:31 +0300

    Save lines list instead of one line
    + add error-file message

--- process.py
+++ process.py
@@ -6,20 +6,25 @@ INCLUDE_DIRECTIVE = "#include"
 CURRENT_PATH = os.path.dirname(__file__)
 
 def process_file(filename, base_path=CURRENT_PATH):
-    filepath = os.path.join(base_path, filename)
-    file = open(filepath, 'r')
-    result = ''
-    line = ''
-    for line in file:
-        if line.strip().startswith(INCLUDE_DIRECTIVE):
-            include_file_path = get_relpath(line)
-            include_file_dir, include_file_name = os.path.split(include_file_path)
-            next_base_path = os.path.join(base_path, include_file_dir)
-            line = process_file(include_file_name, next_base_path)
-        result = result + line
-    file.close()
-    if line != '\n':
-        result = result + '\n'
+    try:
+        filepath = os.path.join(base_path, filename)
+        file = open(filepath, 'r')
+        result = []
+        for line in file:
+            if line.strip().startswith(INCLUDE_DIRECTIVE):
+                include_file_path = get_relpath(line)
+                include_file_dir, include_file_name = os.path.split(include_file_path)
+                next_base_path = os.path.join(base_path, include_file_dir)
+                line = process_file(include_file_name, next_base_path)
+                for x in line:
+                    result.append(x)
+            else:
+                result.append(line)
+        file.close()
+    except:
+        print("Error processing file: " + filepath)
+    if (len(result) > 0) and result[len(result)-1] != '\n':
+        result.append('\n')
     return result
 
 def get_relpath(line):
@@ -29,9 +34,9 @@ def get_relpath(line):
     result = line.rstrip('\n').strip('"').replace('/', os.sep)
     return result
 
-def write_result(output_filename, result):
+def write_result(output_filename, output):
     file = open(output_filename, 'w')
-    file.write(result)
+    file.writelines(output)
     file.close()
 
 if __name__ == "__main__":
